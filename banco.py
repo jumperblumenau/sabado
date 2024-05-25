5bbc4d459dd7d2bbd11cd0d8faa9ad39a8aa005d
jumperblumenau <jumperblumenau2018@gmail.com>
2024-05-25 10:39:43 -0300

    Banco
    Não comitar aqui!

---
+++ banco.py
@@ -0,0 +1,186 @@
+import sqlite3
+from datetime import datetime, timedelta
+import pytz
+import random
+
+# Configuração do banco de dados
+conn = sqlite3.connect('banco_contas.db')
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS contas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    cpf TEXT NOT NULL,
+    agencia TEXT NOT NULL,
+    num_conta TEXT NOT NULL,
+    saldo REAL DEFAULT 0,
+    limite_conta REAL DEFAULT -1000,
+    senha TEXT NOT NULL
+)
+''')
+conn.commit()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS cartoes_credito (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    numero TEXT NOT NULL,
+    titular TEXT NOT NULL,
+    validade TEXT NOT NULL,
+    cod_seguranca INTEGER NOT NULL,
+    limite REAL DEFAULT 5000,
+    limite_disponivel REAL DEFAULT 5000,
+    conta_id INTEGER NOT NULL,
+    FOREIGN KEY (conta_id) REFERENCES contas(id)
+)
+''')
+conn.commit()
+conn.close()
+
+class ContaCorrente:
+    def __init__(self, nome, cpf, agencia, num_conta, senha):
+        self._nome = nome
+        self._cpf = cpf
+        self.saldo = 0
+        self.limite_conta = -1000
+        self.agencia = agencia
+        self.num_conta = num_conta
+        self.transacoes = []
+        self.senha = senha
+        self.cartoes = []
+        self.salvar_conta()
+
+    def salvar_conta(self):
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO contas (nome, cpf, agencia, num_conta, saldo, limite_conta, senha)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''', (self._nome, self._cpf, self.agencia, self.num_conta, self.saldo, self.limite_conta, self.senha))
+        conn.commit()
+        conn.close()
+
+    def __str__(self):
+        return f'''
+        Nome: {self._nome}
+        Cpf: {self._cpf}
+        Saldo: R${self.saldo:.2f}
+        Agência: {self.agencia}
+        Conta: {self.num_conta}
+        Limite Conta: R${self.limite_conta:.2f}
+        clear: {self.clear}
+    '''
+
+    @staticmethod
+    def _data_hora():
+        fuso_BR = pytz.timezone('Brazil/East')
+        horario_BR = datetime.now(fuso_BR)
+        return horario_BR.strftime('%d/%m/%Y %H:%M:%S')
+
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
+        self.atualizar_saldo_banco()
+        print(f"Você depositou R${valor:.2f}. Saldo atual: R${self.saldo:.2f}.")
+
+    def sacar(self, valor):
+        if self.saldo - valor < self.limite_conta:
+            print("Você não tem dinheiro suficiente para realizar esta operação.")
+        else:
+            self.saldo -= valor
+            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
+            self.atualizar_saldo_banco()
+            print(f"Você sacou R${valor:.2f}. Saldo atual: R${self.saldo:.2f}.")
+
+    def consultar_saldo(self):
+        print(f"O saldo atual da conta {self.num_conta} da agência {self.agencia} é: R${self.saldo:.2f}")
+
+    def transferir(self, valor, conta_destino):
+        if self.saldo - valor < self.limite_conta:
+            print("Você não tem dinheiro suficiente para realizar esta operação.")
+        else:
+            self.saldo -= valor
+            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
+            conta_destino.saldo += valor
+            conta_destino.transacoes.append((valor, conta_destino.saldo, ContaCorrente._data_hora()))
+            self.atualizar_saldo_banco()
+            conta_destino.atualizar_saldo_banco()
+            print(f"Você transferiu R${valor:.2f} para {conta_destino._nome}. Saldo atual: R${self.saldo:.2f}")
+
+    def atualizar_saldo_banco(self):
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        cursor.execute('UPDATE contas SET saldo = ? WHERE cpf = ?', (self.saldo, self._cpf))
+        conn.commit()
+        conn.close()
+
+    def historico_transacoes(self):
+        print("Histórico de Transações:")
+        for transacao in self.transacoes:
+            print(transacao)
+
+    def sugerir_emprestimo(self, conta):
+        if conta[5] >= 0:
+            valor_emprestimo = abs(conta[5]) * 2
+            messagebox.showinfo("Empréstimo", f"Você está elegível para um empréstimo de até R${valor_emprestimo:.2f}")
+        else:
+            messagebox.showinfo("Empréstimo", "Você não está elegível para um empréstimo no momento.")
+
+    def gerar_cartao_credito(self):
+        numero = CartaoCredito.gerar_numero_cartao()
+        validade = CartaoCredito.gerar_validade()
+        cod_seguranca = CartaoCredito.gerar_cod_seguranca()
+        cartao = CartaoCredito(self._nome, self, numero, validade, cod_seguranca)
+        return cartao
+
+    def extrato(self):
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM transacoes WHERE conta_id = ?", (self.id,))
+        transacoes = cursor.fetchall()
+        conn.close()
+        return transacoes
+
+
+class CartaoCredito:
+    def __init__(self, titular, conta_corrente, numero, validade, cod_seguranca, limite=5000):
+        self.numero = numero
+        self.titular = titular
+        self.validade = validade
+        self.cod_seguranca = cod_seguranca
+        self.limite = limite
+        self.limite_disponivel = limite
+        self.conta_corrente = conta_corrente
+        conta_corrente.cartoes.append(self)
+
+    @staticmethod
+    def gerar_numero_cartao():
+        return ' '.join([str(random.randint(1000, 9999)) for _ in range(4)])
+
+    @staticmethod
+    def gerar_validade():
+        hoje = datetime.now()
+        validade = hoje + timedelta(days=365*5)
+        return validade.strftime('%m/%Y')
+
+    @staticmethod
+    def gerar_cod_seguranca():
+        return random.randint(100, 999)
+
+    def __str__(self):
+        return f'''
+        Titular: {self.titular}
+        Número: {self.numero}
+        Validade: {self.validade}
+        Código de Segurança: {self.cod_seguranca}
+        Limite: R${self.limite:.2f}
+        Limite Disponível: R${self.limite_disponivel:.2f}
+    '''
+
+    def simular_compra(self, valor):
+        if valor > self.limite_disponivel:
+            print("Compra não aprovada. Limite insuficiente.")
+        else:
+            self.limite_disponivel -= valor
+            self.conta_corrente.transacoes.append((-valor, self.conta_corrente.saldo, ContaCorrente._data_hora()))
+            print(f"Compra de R${valor:.2f} aprovada. Limite disponível: R${self.limite_disponivel:.2f}")
---
+++ interface.py
@@ -0,0 +1,189 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import sqlite3
+from banco import ContaCorrente
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Banco")
+        self.root.geometry("600x600")
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.frame = tk.Frame(self.root)
+        self.frame.pack(pady=20)
+
+        tk.Label(self.frame, text="Nome:").grid(row=0, column=0)
+        self.nome_entry = tk.Entry(self.frame)
+        self.nome_entry.grid(row=0, column=1)
+
+        tk.Label(self.frame, text="CPF:").grid(row=1, column=0)
+        self.cpf_entry = tk.Entry(self.frame)
+        self.cpf_entry.grid(row=1, column=1)
+
+        tk.Label(self.frame, text="Agência:").grid(row=2, column=0)
+        self.agencia_entry = tk.Entry(self.frame)
+        self.agencia_entry.grid(row=2, column=1)
+
+        tk.Label(self.frame, text="Conta:").grid(row=3, column=0)
+        self.conta_entry = tk.Entry(self.frame)
+        self.conta_entry.grid(row=3, column=1)
+
+        tk.Label(self.frame, text="Senha:").grid(row=4, column=0)
+        self.senha_entry = tk.Entry(self.frame, show="*")
+        self.senha_entry.grid(row=4, column=1)
+
+        self.cadastrar_button = tk.Button(self.frame, text="Cadastrar", command=self.cadastrar)
+        self.cadastrar_button.grid(row=5, column=0, columnspan=2, pady=10)
+
+        self.entrar_button = tk.Button(self.frame, text="Entrar", command=self.entrar)
+        self.entrar_button.grid(row=6, column=0, columnspan=2, pady=10)
+
+        self.consultar_contas_button = tk.Button(self.frame, text="Consultar Contas", command=self.consultar_contas)
+        self.consultar_contas_button.grid(row=7, column=0, columnspan=2, pady=10)
+
+        self.consultar_cartao_button = tk.Button(self.frame, text="Consultar Cartão de Crédito",
+                                                 command=self.consultar_cartao)
+        self.consultar_cartao_button.grid(row=8, column=0, columnspan=2, pady=10)
+
+        self.extrato_button = tk.Button(self.frame, text="Ver Extrato", command=self.ver_extrato)
+        self.extrato_button.grid(row=9, column=0, columnspan=2, pady=10)
+
+        self.sair_button = tk.Button(self.frame, text="Sair", command=self.root.destroy)
+        self.sair_button.grid(row=11, column=0, columnspan=2, pady=10)
+
+        self.clear_button = tk.Button(self.frame, text="clear", command=self.clear)
+        self.clear_button.grid(row=10, column=0, columnspan=2, pady=10)
+
+    def cadastrar(self):
+        nome = self.nome_entry.get()
+        cpf = self.cpf_entry.get()
+        agencia = self.agencia_entry.get()
+        conta = self.conta_entry.get()
+        senha = self.senha_entry.get()
+
+        if nome and cpf and agencia and conta and senha:
+            ContaCorrente(nome, cpf, agencia, conta, senha)
+            messagebox.showinfo("Sucesso", "Conta cadastrada com sucesso!")
+        else:
+            messagebox.showerror("Erro", "Todos os campos são obrigatórios.")
+
+    def clear(self):
+        self.nome_entry.delete(0, tk.END)
+        self.cpf_entry.delete(0, tk.END)
+        self.agencia_entry.delete(0, tk.END)
+        self.conta_entry.delete(0, tk.END)
+        self.senha_entry.delete(0, tk.END)
+    def entrar(self):
+        cpf = self.cpf_entry.get()
+        senha = self.senha_entry.get()
+
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM contas WHERE cpf = ? AND senha = ?", (cpf, senha))
+        conta = cursor.fetchone()
+        conn.close()
+
+        if conta:
+            self.mostrar_opcoes(list(conta))  # Converte a tupla para lista
+        else:
+            messagebox.showerror("Erro", "CPF ou senha incorretos.")
+
+    def mostrar_opcoes(self, conta):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+
+        tk.Label(self.root, text=f"Bem-vindo, {conta[1]}!").pack(pady=10)
+
+        tk.Button(self.root, text="Consultar Saldo", command=lambda: self.consultar_saldo(conta)).pack(pady=5)
+        tk.Button(self.root, text="Depositar", command=lambda: self.depositar(conta)).pack(pady=5)
+        tk.Button(self.root, text="Sacar", command=lambda: self.sacar(conta)).pack(pady=5)
+        tk.Button(self.root, text="Transferir", command=lambda: self.transferir(conta)).pack(pady=5)
+        tk.Button(self.root, text="Sugerir Empréstimo", command=lambda: self.sugerir_emprestimo(conta)).pack(pady=5)
+
+    def consultar_saldo(self, conta):
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT saldo FROM contas WHERE id = ?", (conta[0],))
+        saldo_atualizado = cursor.fetchone()[0]
+        conn.close()
+        messagebox.showinfo("Saldo", f"Saldo atual: R${saldo_atualizado:.2f}")
+
+    def depositar(self, conta):
+        valor = float(simpledialog.askstring("Depositar", "Valor:"))
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        novo_saldo = conta[5] + valor
+        cursor.execute("UPDATE contas SET saldo = ? WHERE id = ?", (novo_saldo, conta[0]))
+        conn.commit()
+        conn.close()
+        conta[5] = novo_saldo  # Atualiza o saldo no objeto em memória
+        messagebox.showinfo("Sucesso", f"Você depositou R${valor:.2f}. Saldo atual: R${novo_saldo:.2f}")
+
+    def sacar(self, conta):
+        valor = float(simpledialog.askstring("Sacar", "Valor:"))
+        if conta[5] - valor < conta[6]:
+            messagebox.showerror("Erro", "Você não tem dinheiro suficiente para realizar esta operação.")
+        else:
+            conn = sqlite3.connect('banco_contas.db')
+            cursor = conn.cursor()
+            novo_saldo = conta[5] - valor
+            cursor.execute("UPDATE contas SET saldo = ? WHERE id = ?", (novo_saldo, conta[0]))
+            conn.commit()
+            conn.close()
+            conta[5] = novo_saldo  # Atualiza o saldo no objeto em memória
+            messagebox.showinfo("Sucesso", f"Você sacou R${valor:.2f}. Saldo atual: R${novo_saldo:.2f}")
+
+    def transferir(self, conta):
+        conta_destino_num = simpledialog.askstring("Transferir", "Conta de destino:")
+        valor = float(simpledialog.askstring("Transferir", "Valor:"))
+
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM contas WHERE num_conta = ?", (conta_destino_num,))
+        conta_destino = cursor.fetchone()
+
+        if conta_destino:
+            if conta[5] - valor < conta[6]:
+                messagebox.showerror("Erro", "Você não tem dinheiro suficiente para realizar esta operação.")
+            else:
+                novo_saldo = conta[5] - valor
+                novo_saldo_destino = conta_destino[5] + valor
+                cursor.execute("UPDATE contas SET saldo = ? WHERE id = ?", (novo_saldo, conta[0]))
+                cursor.execute("UPDATE contas SET saldo = ? WHERE id = ?", (novo_saldo_destino, conta_destino[0]))
+                conn.commit()
+                conta[5] = novo_saldo  # Atualiza o saldo no objeto em memória
+                messagebox.showinfo("Sucesso",
+                                    f"Você transferiu R${valor:.2f} para a conta {conta_destino[4]}. Saldo atual: R${novo_saldo:.2f}")
+                conn.close()
+    def sugerir_emprestimo(self, conta):
+        if conta[5] >= 0:
+            valor_emprestimo = abs(conta[5]) * 2
+            messagebox.showinfo("Empréstimo", f"Você está elegível para um empréstimo de até R${valor_emprestimo:.2f}")
+        else:
+            messagebox.showinfo("Empréstimo", "Você não está elegível para um empréstimo no momento.")
+
+    def consultar_contas(self):
+        conn = sqlite3.connect('banco_contas.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM contas")
+        contas = cursor.fetchall()
+        conn.close()
+
+        contas_str = "\n".join([f"Nome: {conta[1]}, CPF: {conta[2]}, Agência: {conta[3]}, Conta: {conta[4]}, Saldo: R${conta[5]:.2f}" for conta in contas])
+        messagebox.showinfo("Contas Cadastradas", contas_str)
+
+    def consultar_cartao(self):
+        cartao = self.conta_corrente.gerar_cartao_credito()
+        messagebox.showinfo("Cartão de Crédito", str(cartao))
+
+    def ver_extrato(self):
+        extrato = self.conta_corrente.extrato()
+        extrato_str = "\n".join(
+            [f"{transacao[2]}: {transacao[0]} - Saldo: R${transacao[1]:.2f}" for transacao in extrato])
+        messagebox.showinfo("Extrato", extrato_str)
+
+    def consultar_limite_disponivel(self):
+        limite = self.conta_corrente.consultar_limite_disponivel()
+        messagebox.showinfo("Limite Disponível", f"Limite Disponível: R${limite:.2f}")
